4925582cb9e61f71a28aea213e540e006ff070b5
er9966 <70611850+er9966@users.noreply.github.com>
2020-09-09 13:47:12 -0700

    Update GreenScreenProblem.js

--- GreenScreenProblem.js
+++ GreenScreenProblem.js
@@ -5,11 +5,12 @@ var width = fgImage.getWidth();
 var output = new SimpleImage(width, height);
 
 for (var px of fgImage.values()){
+    var x = px.getX(); //find x position
+    var y = px.getY();//find y position
+    
     //define green: has more green than blue and red combined
     if(px.getGreen() > (px.getRed() + px.getBlue())){  
         //look at same position in bgImage
-        var x = px.getX(); //find x position
-        var y = px.getY();//find y position
         var bgPx = bgImage.getPixel(x, y); // get same position in bgImage
         output.setPixel(x, y, bgPx);
     } else {
